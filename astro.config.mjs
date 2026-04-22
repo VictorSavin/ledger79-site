0748aedf47635c0a7fcf708398591eec64e6b771
Victor S <victorsavin@Victors-iMac.lan>
2026-04-22 18:24:56 +0100

    Add sitemape and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: "https://ledger79.com",
   output: "server",
   adapter: vercel(),
+  integrations: [sitemap()],
 });
